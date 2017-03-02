a3bb3d3bc2f480b3bf7a8c88c69f0ff543a21466
Matt Yoder <myoder@ccrpc.org>
2017-03-02 09:15:01 -0600

    Added geo service.

---
+++ src/app/geo.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+
+Injectable()
+export class Geo {
+  static BG_DEFAULT_SETTINGS = {
+		activityType: 'OtherNavigation', // iOS activity type
+		autoSync: false, // Do not automatically post to the server
+		debug: false, // Disable debug notifications
+		desiredAccuracy: 0, // Overridden by settings.
+		distanceFilter: 20, // Generate update events every 20 meters
+		disableElasticity: false, // Auto-adjust distanceFilter
+		fastestLocationUpdateInterval: 1000, // Prevent updates more than once per second (Android)
+		locationUpdateInterval: 5000, // Request updates every 5 seconds (Android)
+		startOnBoot: false, // Do not start tracking on device boot
+		stationaryRadius: 20, // Activate the GPS after 20 meters (iOS)
+		stopOnTerminate: true, // Stop geolocation tracking on app exit
+		stopTimeout: 3 // Keep tracking for 3 minutes while stationary
+	};
+  private bgGeo: any;
+  public isRecording: boolean = false;
+
+  init() {
+    this.bgGeo = (<any>window).BackgroundGeolocation;
+  }
+
+  setRecording(recording: boolean) {
+    this.isRecording = recording;
+  }
+}
