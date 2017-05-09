20e21ad4cc55a193b0b973ffddcfc0561bbcd579
Matt Yoder <myoder@ccrpc.org>
2017-05-09 16:27:58 -0500

    Added dismiss action.

--- src/pages/tutorial/tutorial.ts
+++ src/pages/tutorial/tutorial.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { ViewController } from 'ionic-angular';
 
 @Component({
   selector: 'page-tutorial',
   templateUrl: 'tutorial.html'
 })
 export class TutorialPage {
+  constructor(
+    private viewCtrl: ViewController) {}
 
+  private dismiss() {
+    this.viewCtrl.dismiss();
+  }
 }
