b83f5040c8e4105f3065ad2dd7f70d84c882b47c
Matt Yoder <myoder@ccrpc.org>
2017-05-30 15:40:31 -0500

    Prepare for beta release.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Settings } from './settings';
 import { State } from './state';
 import { Storage } from './storage';
 import { TabsPage } from '../pages/tabs/tabs';
+import { DEBUG } from './config';
 
 @Component({
   templateUrl: 'app.html'
@@ -36,7 +37,7 @@ export class BikeMoves {
       this.ensureGeolocation();
       this.legacy.upgrade();
 
-      (window as any).storage = storage;
+      if (DEBUG) (window as any).storage = storage;
     });
   }
 
